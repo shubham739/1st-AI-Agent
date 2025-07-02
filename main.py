8becbc3e6e793fed692c0fab8b94b1e56cde9225
Shubham <shubhamtanwar72@gmail.com>
2025-07-02 19:38:42 +0530

    agent with the prompt

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_core.runnables.base import RunnableConfig
 
 load_dotenv()
 
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    toold_used: list[str]
+
+# Initialize the LLM with OpenAI's GPT-3.5 Turbo model
 llm = ChatOpenAI(model="gpt-3.5-turbo")
-response = llm.invoke("What is the capital of France?")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm, 
+    prompt=prompt, 
+   tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+
+print("Raw response:", raw_response)
+
+structured_response = parser.parse(raw_response.get("output"))
+print("Structured response:", structured_response)
